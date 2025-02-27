b9b15a531ccd37f4b63a8209c774c9ae73b34d95
nickprbs <np@rushsoft.de>
2025-02-27 18:52:17 +0100

    started visualization builder

---
+++ src/pipeline/visualization_builder.py
@@ -0,0 +1,14 @@
+"""
+Visualization Builder for the Bachelor Research Project "Gamepad Controls for Visualizations"
+This file generates the Vega-Lite Code used for the three connected Visualizations.
+Run as main to generate the JSON code.
+"""
+
+# Imports
+import altair as alt
+import pandas as pd
+import geopandas as gpd
+
+if __name__ == "__main__":
+    
+    pass
